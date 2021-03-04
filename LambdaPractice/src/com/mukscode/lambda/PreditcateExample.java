26870cb0cc8184badc368bff6fe1f8aa2004571b
MukeshKumar <MukeshKumar@DESKTOP-5UCTPUC>
2021-03-04 22:59:39 +0530

    Predicate Example

--- LambdaPractice/src/com/mukscode/lambda/PreditcateExample.java
+++ LambdaPractice/src/com/mukscode/lambda/PreditcateExample.java
@@ -3,8 +3,9 @@ package com.mukscode.lambda;
 public class PreditcateExample {
 
 	public static void main(String[] args) {
-		
 
+		Predicate<Integer> checkAgeGreaterThan10 = i -> i>10;
+		System.out.println(checkAgeGreaterThan10(100)); //true
 	}
-
 }
+
